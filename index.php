c29852f4e82adaca4987a67a641f84a29cbb376b
Michelle <mbojorquez4@cnm.edu>
2017-07-17 08:21:11 -0600

    added persona

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Amber
+		       26 yrs old
+		       MOther Personal assistant</title>
+	</head>
+	<body><p> Amber is 26 years old. She is a personal assistant for a real-estate agent. She has been married to her
+			husband Brain for 5 years and they have a 3 year old daughter named Lindsey. She mostly uses her I phone for
+			all her technical  needs. Amber makes custom baby bibs on the weekends and in her free time. She started
+			selling bibs to her friends and family and thinks she could make a bigger profit if she could get more people
+			to see her product.
+			Frustrations
+			Amber feels frustratrated with the negative feed back customers give because of the websites customer service
+			inability to mediate problems between other buyers and sellers. She feels like this drives business elsewhere.
+			Also because esty now allows manufactured products on the site she feels she has loss sales because of it.
+			Amber joined the site with hopes of doing something she enjoys, making some extra money and clothing pudgy
+			little babies with stylish and chic bibs.
+			Frustrations
+			Amber feels frustratrated with the negative feed back customers give because of the websites customer service
+			inability to mediate problems between other buyers and sellers. She feels like this drives business elsewhere.
+			Also because esty now allows manufactured products on the site she feels she has loss sales because of it.
+			Amber joined the site with hopes of doing something she enjoys, making some extra money and clothing pudgy
+			little babies with stylish and chic bibs.
+			'Need to know'
+			Amber works a full time job and is a busy mother, she appreciates easy to use templates to build her online
+			store. A mobile user friendly site with options to be creative and productive is very helpful to her lifestyle.
+			Scenarios
+			Saturday midmorning Amber is doing the weekly family grocery shopping, she receives an alert on her phone about
+			a purchases made on her esty site. She is absolutely thrilled that she can execute selling her product online
+			without it interfering with her daily activities.
+
+		</p>
+		</body>
