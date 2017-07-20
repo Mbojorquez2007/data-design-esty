db9f530394a91c14a3510818417b5c2c1b9c60d5
Michelle <mbojorquez4@cnm.edu>
2017-07-20 16:53:05 -0600

    checkpoint commit ordered by @deepdivedylan

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,16 +1,9 @@
--- this is a comment in SQL (yes, the space is needed!)
--- these statements will drop the tables and re-add them
--- this is akin to reformatting and reinstalling Windows (OS X never needs a reinstall...) ;)
--- never ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever
--- do this on live data!!!!
 DROP TABLE IF EXISTS `item`;
 DROP TABLE IF EXISTS seller;
 
--- the CREATE TABLE function is a function that takes tons of arguments to layout the table's schema
+
 CREATE TABLE seller (
-	-- this creates the attribute for the primary key
-	-- auto_increment tells mySQL to number them {1, 2, 3, ...}
-	-- not null means the attribute is required!
+
 	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileActivationToken CHAR(32),
 	profileAtHandle VARCHAR(32) NOT NULL,
@@ -22,13 +15,12 @@ CREATE TABLE seller (
 	profileSalt CHAR(64) NOT NULL,
 	UNIQUE(profileEmail),
 	UNIQUE(profileAtHandle),
-	-- this officiates the primary key for the entity
+
 	PRIMARY KEY(profileId)
 );
 
 -- create the tweet entity
 CREATE TABLE item (
-	-- this is for yet another primary key...
 	itemId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	-- this is for a foreign key; auto_incremented is omitted by design
 	itemProfileId INT UNSIGNED NOT NULL,
