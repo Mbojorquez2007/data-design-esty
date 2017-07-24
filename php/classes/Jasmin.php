d8a9568780a499188d725b29ebbf102fcb1defcd
Michelle <mbojorquez4@cnm.edu>
2017-07-24 16:25:38 -0600

    checkpoint commit ordered by @deepdivedylan

---
+++ php/classes/Jasmin.php
@@ -0,0 +1,25 @@
+<?php
+namespace Edu\Cnm\DataDesign;
+
+require_once("autoload.php");
+
+/**
+ *Small Cross Section of profile information
+ *
+ *This information can be considered a small example of what profile stores when information are sent and
+ * and recieved using this information. This can be easily be extended to emulate
+ * more features in a profile.
+ * @author Michelle Allen <mbojorquez4@cnm.edu>
+ **/
+class ProfileId{
+	/**
+	 * id for this profile; this is the primary key
+	 * @var int $ProfileId
+	 **/
+	private $ProfileId;
+	/**
+	 *id for the user that owns this profile; this a forgien key
+	 */
+
+
+}
